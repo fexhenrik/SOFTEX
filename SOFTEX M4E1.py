3791fb19a255c1f66281e35224a6ede954b35b29
fexhenrik <99749107+fexhenrik@users.noreply.github.com>
2022-08-15 21:16:19 -0300

    Update SOFTEX M4E1.py

--- SOFTEX M4E1.py
+++ SOFTEX M4E1.py
@@ -1,38 +1,35 @@
 class Album:
-  def __init__(self, artist, title, genre, tracks):
+  def __init__(self, artist, title, genre):
     self.artist = artist
     self.title = title
     self.genre = genre
-    self.tracks = tracks
 
   def exibir(self):
-    print(self.artist, self.title, self.genre, self.tracks)
+    print(self.artist, self.title, self.genre)
 
-album = Album("Artist: Tayor Swift", "\nTitle: RED (taylor's version)", "\nGenre: Country-pop", "\nTracks: 30 tracks")
+album = Album("Artist: Tayor Swift", "\nTitle: RED (taylor's version)", "\nGenre: Country-pop")
 album.exibir()
 
 class Book:
-  def __init__(self, author, title, genre, pages):
+  def __init__(self, author, title, genre):
     self.author = author
     self.title = title
     self.genre = genre
-    self.pages = pages
 
   def exibir1(self):
-    print(self.author, self.title, self.genre, self.pages)
+    print(self.author, self.title, self.genre)
 
-book = Book("\nAuthor: Rick Riordan", "\nTitle: Percy Jackson and The Last Olympian", "\nGenre: Fantasy, YA", "\nPages: 381")
+book = Book("\nAuthor: Rick Riordan", "\nTitle: Percy Jackson and The Last Olympian", "\nGenre: Fantasy, YA")
 book.exibir1()
 
 class Show:
-  def __init__(self, name, director, seasons, episodes):
+  def __init__(self, name, director, seasons):
     self.name = name
     self.director = director
     self.seasons = seasons
-    self.episodes = episodes
 
   def exibir0(self):
-    print(self.name, self.director, self.seasons, self.episodes)
+    print(self.name, self.director, self.seasons)
 
-show = Show("\nName: Fleabag", "\nDirector: Phoebe Waller-Bridge", "\n Seasons: 2", "\nEpisodes: 12")
+show = Show("\nName: Fleabag", "\nDirector: Phoebe Waller-Bridge", "\n Seasons: 2")
 show.exibir0()
